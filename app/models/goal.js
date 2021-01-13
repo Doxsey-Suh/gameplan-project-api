9084cbe5af31366938f61cf6df1953e5b5ba9f16
kdoxsey <kdoxse@gmail.com>
2021-01-12 19:14:45 -0500

    added 5 optional steps to the goal model

--- app/models/goal.js
+++ app/models/goal.js
@@ -5,6 +5,31 @@ const goalSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  step1: {
+    type: Array,
+    ref: 'User',
+    required: false
+  }, 
+    step2: {
+    type: Array,
+    ref: 'User',
+    required: false
+  }, 
+    step3: {
+    type: Array,
+    ref: 'User',
+    required: false
+  }, 
+    step4: {
+    type: Array,
+    ref: 'User',
+    required: false
+  }, 
+    step5: {
+    type: Array,
+    ref: 'User',
+    required: false
+  }, 
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
